a874fd829b0918fb60ae1712b9369632e6df636f
James Baster <james.baster@opendataservices.coop>
2018-12-13 08:54:48 +0000

    Make upgrade db work - delete commands that are nunneded curretly (tho we will have to try and put some of them back later)

--- ocdskingfisher/cli/commands/base.py
+++ ocdskingfisher/cli/commands/base.py
@@ -1,5 +1,3 @@
-import ocdskingfisher.sources_util
-import ocdskingfisher.database
 
 
 class CLICommand:
@@ -14,27 +12,3 @@ class CLICommand:
 
     def run_command(self, args):
         pass
-
-    def configure_subparser_for_selecting_existing_collection(self, subparser):
-        subparser.add_argument("source", help="source")
-        subparser.add_argument("--dataversion", help="Specify a data version")
-        subparser.add_argument("--sample", help="Sample source only", action="store_true")
-
-    def run_command_for_selecting_existing_collection(self, args):
-
-        sources = ocdskingfisher.sources_util.gather_sources()
-
-        sample_mode = args.sample
-        data_version = args.dataversion
-
-        if args.source not in sources:
-            print("We can not find the source that you requested!")
-            quit(-1)
-
-        # This will raise an error if the version specified does not exist on disk.
-        self.collection_instance = sources[args.source](self.config.data_dir,
-                                                        remove_dir=False,
-                                                        sample=sample_mode,
-                                                        data_version=data_version,
-                                                        new_version=False
-                                                        )
